94c5ecf18a501c18e8eaf8427a09827ec379c2af
Yong Wu <13618379679@163.com>
2023-06-13 20:38:32 +0800

    打包发布新的nuget版本

--- src/OpenDeepSpace.Autofacastle/AspectAttention/InterceptorPoint/Attributes/InterceptPointAttribute.cs
+++ src/OpenDeepSpace.Autofacastle/AspectAttention/InterceptorPoint/Attributes/InterceptPointAttribute.cs
@@ -1,4 +1,4 @@
-﻿using OpenDeepSpace.NetCore.Autofacastle.Extensions;
+﻿using OpenDeepSpace.Autofacastle.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- src/OpenDeepSpace.Autofacastle/Extensions/StringExtensions.cs
+++ src/OpenDeepSpace.Autofacastle/Extensions/StringExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenDeepSpace.NetCore.Autofacastle.Extensions
+namespace OpenDeepSpace.Autofacastle.Extensions
 {
     /// <summary>
     /// 
